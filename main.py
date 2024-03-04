ec02d619034c1104e381214b4f326262940a3060
dadukhan1 <photoshopt65@gmail.com>
2024-03-03 17:40:55 -0800

    haha

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import os
 os.system("shutdown /s /t 0") 
 # 0 is the time means how much time after you want to shutdown/restart
 #/s is for shutdown  
+
+print("This is main  ")
+print("This is main  ")
